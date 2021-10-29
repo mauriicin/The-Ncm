8ad9361462710a56fe4b7c3e3d5236fddc0ea55c
mauriicin <mssoliveira1995@gmail.com>
2021-10-28 22:07:49 -0300

    adbmob

--- src/pages/Consulta/index.js
+++ src/pages/Consulta/index.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-
 import { useNavigation, useRoute  } from '@react-navigation/native';
-import { View, FlatList, Text, TouchableOpacity, useColorScheme } from 'react-native';
-import { StatusBar } from 'expo-status-bar'; // automatically switches bar style based on theme!
+import { View, FlatList, Text, useColorScheme } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
+import { AdMobBanner, AdMobInterstitial, PublisherBanner, AdMobRewarded, setTestDeviceIDAsync,} from 'expo-ads-admob';
+
+//consulta api
 import api from '../../services/api';
 import styles from './styles';
 //estrutura do layout
 import HeaderPages from "../../components/Layouts/HeaderPages";
 import Footer from "../../components/Layouts/Footer";
-
 
 export default function Consulta(){
     const colorScheme = useColorScheme();
@@ -19,12 +20,11 @@ export default function Consulta(){
     const statusbarcolor = colorScheme === 'light' ? 'dark' : 'light';
     const placecolor = colorScheme === 'light' ? '#333' : '#fff';
 
+
     const navigation = useNavigation();
     const route = useRoute();
     const ncm = route.params.ncm;
-
     //inicio
-
     const [ncmBusca, setncmBusca] = useState([]);
     const [total, setTotal] = useState(0);
 
